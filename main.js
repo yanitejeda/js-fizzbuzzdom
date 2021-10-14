6083340eabf73c45bbf58796f8f8de97dbdcab23
yanitejeda <tejedasp@gmail.com>
2021-10-14 19:21:22 +0200

    completamento esercizio

--- main.js
+++ main.js
@@ -2,25 +2,41 @@
 const tittle = document.querySelector('h1');
 tittle.style.textAlign = 'center';
 
-const scacchieraContainer = document.getElementById('container_scacchiera');
+
+//dichiaro l'elemento nel quale voglio inserire i miei dati
+const boxes= document.getElementsByClassName('box');
+console.log(boxes);
 
 
 
+//conto quantola lunghezza dei mio container ovvero fino all'ultimo box
+for (let i = 1; i <= boxes.length; i++){
+    
+
+    //faccio partire a cotare da 0 la mia variabile 
+    //di conseguenza devo togliere 1 a boxes
+    let square = boxes[i-1];
 
 
+    if ( i % 3 ===0 && i % 5 === 0){
+        square.classList.add('x3-5');
+        square.innerHTML = 'fizzbuzz';
 
 
-for (let i = 1; i <= 10; i++){
+    } else if (i % 3 === 0 ){
+        square.classList.add('x5');
+        square.innerHTML = 'buzz'
 
-    console.log('tabellina del', i);
-    scacchieraContainer.innerHTML += `<div class='col'>
-                                    <h4>${i}</h4>
-                                        </div>`;
+        
+    }else if (i % 5 === 0){
+        square.classList.add('x3');
+        square.innerHTML = 'fizz'
+
+        
+    } else{
+        square.classList.add('std');
+        square.innerHTML = i;
+
+    }
 }
-
-
-
-
-
-
 
